905a4bff903f43e8863d391b000decd514f7bcbd
Maria Smith <maria.t.smith023@gmail.com>
2025-11-12 21:52:30 -0500

    complete Invoice class

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Invoice {
+    private ArrayList<LineItem> lineItems;
+
+    // Constructor
+    public Invoice (ArrayList<LineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
+
+    // Getters
+    public ArrayList<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    // Methods
+    public void addLineItem (LineItem lineItem) {
+        lineItems.add(lineItem);
+    }
+
+    public double getTotal () {
+        double total = 0;
+        for (LineItem lineItem : lineItems) {
+            total += lineItem.getTotal();
+        }
+
+        return total;
+    }
 }
