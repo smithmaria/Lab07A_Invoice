da2a17fe0b2f2b0ae0465a5bd0e7016de747f25f
Maria Smith <maria.t.smith023@gmail.com>
2025-11-12 21:25:01 -0500

    setup files

---
+++ src/InvoiceFrame.java
@@ -0,0 +1,4 @@
+import javax.swing.*;
+
+public class InvoiceFrame extends JFrame {
+}
---
+++ src/InvoiceRunner.java
@@ -0,0 +1,13 @@
+/**
+ * Runner class for Invoice
+ * @author mariasmith
+ */
+public class InvoiceRunner {
+    /**
+     * Main method to launch Invoice
+     * @param args
+     */
+    public static void main(String[] args) {
+        InvoiceFrame frame = new InvoiceFrame();
+    }
+}
