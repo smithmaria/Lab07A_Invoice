12794d17e5d4def161de1f9fbd8574a09f5137d6
Maria Smith <maria.t.smith023@gmail.com>
2025-11-12 21:32:47 -0500

    complete product class

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,19 @@
 public class Product {
+    private String name;
+    private double unitPrice;
+
+    // Constructor
+    public Product(String name, double unitPrice) {
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
 }
