b58233030879e60fa767e06384110d8b79aa6fcb
Maria Smith <maria.t.smith023@gmail.com>
2025-11-12 21:38:02 -0500

    complete LineItem class

--- src/LineItem.java
+++ src/LineItem.java
@@ -1,2 +1,25 @@
 public class LineItem {
+    private Product product;
+
+    private int quantity;
+
+    // Constructor
+    public LineItem (Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    // Getters
+    public Product getProduct () {
+        return product;
+    }
+
+    public int getQuantity () {
+        return quantity;
+    }
+
+    // Methods
+    public double getTotal () {
+        return (quantity * product.getUnitPrice());
+    }
 }
